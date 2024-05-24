4d393b6d17c03a2e151a51716f6fbae455413b79
kilnonedre <1600513615@qq.com>
2024-05-24 17:00:56 +0800

    ci: 基础框架搭建

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+// prettier，修改prettier文件时需要重新打开项目，才能生效
+module.exports = {
+  tabWidth: 2,
+  printWidth: 80,
+  endOfLine: 'auto',
+  arrowParens: 'avoid',
+  trailingComma: 'es5',
+  semi: false,
+  useTabs: false,
+  singleQuote: true,
+  bracketSpacing: true
+}
