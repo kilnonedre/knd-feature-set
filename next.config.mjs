84d7a0817d52f6be2736d71be71e2ed0a89744e8
kilnonedre <1600513615@qq.com>
2024-05-24 18:09:55 +0800

    ci: [base] 引入 sass

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const nextConfig = {
+  reactStrictMode: true,
+  sassOptions: {
+    additionalData: `@import '@/style/indexStyle.scss';`,
+  },
+}
+
+export default nextConfig
